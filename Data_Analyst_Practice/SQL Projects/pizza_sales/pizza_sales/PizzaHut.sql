8272dcb662d76dedac923d97200504cae2b30c19
Mr.Sahil <sahilbhavsar1255@gmail.com>
2026-02-26 12:07:10 +0530

    update PizzaHut

--- Data_Analyst_Practice/SQL Projects/pizza_sales/pizza_sales/PizzaHut.sql
+++ Data_Analyst_Practice/SQL Projects/pizza_sales/pizza_sales/PizzaHut.sql
@@ -101,7 +101,22 @@ order by revenue desc
 
 --Advanced:
 --Calculate the percentage contribution of each pizza type to total revenue.
-
+select  pt.category, 
+round(
+sum(od.quantity * p.price)
+	/
+	(select 
+round(sum(od.quantity * p.price),2) as revenue
+from order_details od
+join pizzas p
+on od.pizza_id = p.pizza_id) * 100,2) as revenue
+from pizza_types pt
+join pizzas p
+on pt.pizza_type_id = p.pizza_type_id
+join order_details od
+on od.pizza_id = p.pizza_id
+group by pt.category
+order by revenue desc
 
 --Analyze the cumulative revenue generated over time.
 --Determine the top 3 most ordered pizza types based on revenue for each pizza category.
