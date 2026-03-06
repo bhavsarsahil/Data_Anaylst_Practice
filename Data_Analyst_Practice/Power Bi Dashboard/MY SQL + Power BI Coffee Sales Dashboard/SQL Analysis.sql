626a8bba1d5a98d50bc84d7d831739b07fe61789
Mr.Sahil <sahilbhavsar1255@gmail.com>
2026-03-06 10:52:45 +0530

    write query in sql file

--- Data_Analyst_Practice/Power Bi Dashboard/MY SQL + Power BI Coffee Sales Dashboard/SQL Analysis.sql
+++ Data_Analyst_Practice/Power Bi Dashboard/MY SQL + Power BI Coffee Sales Dashboard/SQL Analysis.sql
@@ -43,3 +43,110 @@ GROUP BY
 ORDER BY 
     MONTH(transaction_date);
 
+
+ --TOTAL ORDERS
+SELECT COUNT(transaction_id) as Total_Orders
+FROM coffee_shop_sales 
+WHERE MONTH (transaction_date)= 5 -- for month of (CM-May)
+
+--TOTAL ORDERS KPI - MOM DIFFERENCE AND MOM GROWTH
+SELECT  
+    MONTH(transaction_date) AS month,
+    ROUND(COUNT(transaction_id),0) AS total_orders,
+   (COUNT(transaction_id) - LAG(COUNT(transaction_id), 1) 
+   OVER (ORDER BY MONTH(transaction_date))) * 100.0 / 
+   LAG(COUNT(transaction_id), 1) OVER (ORDER BY MONTH(transaction_date)) AS mom_increase_percentage
+FROM 
+    coffee_shop_sales
+WHERE 
+    MONTH(transaction_date) IN (4, 5) -- for April and May
+GROUP BY 
+    MONTH(transaction_date)
+ORDER BY 
+    MONTH(transaction_date);
+
+
+--TOTAL QUANTITY SOLD
+SELECT SUM(transaction_qty) as Total_Quantity_Sold
+FROM coffee_shop_sales 
+WHERE MONTH(transaction_date) = 5 -- for month of (CM-May)
+
+--TOTAL QUANTITY SOLD KPI - MOM DIFFERENCE AND MOM GROWTH
+SELECT 
+    MONTH(transaction_date) AS month,
+    ROUND(SUM(transaction_qty),0) AS total_quantity_sold,
+    -- Multiply by 100.0 first to ensure decimal results
+    (SUM(transaction_qty) - LAG(SUM(transaction_qty), 1) OVER (ORDER BY MONTH(transaction_date))) * 100.0 / 
+    LAG(SUM(transaction_qty), 1) OVER (ORDER BY MONTH(transaction_date)) AS mom_increase_percentage
+FROM 
+    coffee_shop_sales
+WHERE 
+    MONTH(transaction_date) IN (4, 5)
+GROUP BY 
+    MONTH(transaction_date)
+ORDER BY 
+    MONTH(transaction_date);
+
+--CALENDAR TABLE – DAILY SALES, QUANTITY and TOTAL ORDERS
+SELECT
+    SUM(unit_price * transaction_qty) AS total_sales,
+    SUM(transaction_qty) AS total_quantity_sold,
+    COUNT(transaction_id) AS total_orders
+FROM 
+    coffee_shop_sales
+WHERE 
+    transaction_date = '2023-05-18'; --For 18 May 2023
+
+--If you want to get exact Rounded off values then use below query to get the result:
+SELECT 
+    CONCAT(ROUND(SUM(unit_price * transaction_qty) / 1000.0, 1), 'K') AS total_sales,
+    CONCAT(ROUND(COUNT(transaction_id) / 1000.0, 1), 'K') AS total_orders,
+    CONCAT(ROUND(SUM(transaction_qty) / 1000.0, 1), 'K') AS total_quantity_sold
+FROM 
+    coffee_shop_sales
+WHERE 
+    transaction_date = '2023-05-18';
+
+
+SELECT 
+    CASE 
+        WHEN DATEPART(weekday, transaction_date) IN (1, 7) THEN 'Weekends'
+        ELSE 'Weekdays'
+    END AS day_type,
+    SUM(unit_price * transaction_qty) AS total_sales
+FROM 
+    coffee_shop_sales
+WHERE 
+    MONTH(transaction_date) = 5
+GROUP BY 
+    CASE 
+        WHEN DATEPART(weekday, transaction_date) IN (1, 7) THEN 'Weekends'
+        ELSE 'Weekdays'
+    END;
+
+--SALES BY STORE LOCATION
+SELECT 
+	store_location,
+	SUM(unit_price * transaction_qty) as Total_Sales
+FROM coffee_shop_sales
+WHERE
+	MONTH(transaction_date) =5 
+GROUP BY store_location
+ORDER BY Total_Sales DESC
+
+select avg(unit_price*transaction_qty) as Avg_sales
+from coffee_shop_sales
+where month(transaction_date) = 5
+
+--SALES TREND OVER PERIOD
+SELECT AVG(total_sales) AS average_sales
+FROM (
+    SELECT 
+        SUM(unit_price * transaction_qty) AS total_sales
+    FROM 
+        coffee_shop_sales
+	WHERE 
+        MONTH(transaction_date) = 5  -- Filter for May
+    GROUP BY 
+        transaction_date
+) AS internal_query;
