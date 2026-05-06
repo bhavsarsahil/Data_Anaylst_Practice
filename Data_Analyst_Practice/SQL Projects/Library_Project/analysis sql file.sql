aa9128dd6d315de2e238c82ba333a0dbda8c50fb
Mr.Sahil <sahilbhavsar1255@gmail.com>
2026-05-06 17:25:01 +0530

    complate task

--- Data_Analyst_Practice/SQL Projects/Library_Project/analysis sql file.sql
+++ Data_Analyst_Practice/SQL Projects/Library_Project/analysis sql file.sql
@@ -75,60 +75,97 @@ WHERE reg_date >= CURRENT_DATE - INTERVAL 180 DAY;
 INSERT into members(member_id,member_name,member_address,reg_date)
 VALUES
 ('C120','Smith', '456 Birch St', '2026-03-05'),
-('C121','Smith', '123 Main St', '2026-02-05')
+('C121','Smith', '123 Main St', '2026-02-05');
 
 -- Task 10: List Employees with Their Branch Manager's Name and their branch details**:
-
+SELECT 
+    e1.*,
+    b.manager_id, 
+    e2.emp_name AS manager_name
+FROM employees e1
+JOIN branch b ON e1.branch_id = b.branch_id
+JOIN employees e2 ON b.manager_id = e2.emp_id;
 
 -- Task 11. Create a Table of Books with Rental Price Above a Certain Threshold
+SELECT * FROM Books
+WHERE rental_price > 7;
 
 -- Task 12: Retrieve the List of Books Not Yet Returned
-
+select DISTINCT ist.issued_book_name
+from issued_status as ist
+LEFT JOIN return_status rs
+on ist.issued_id=rs.issued_id
+where rs.return_id is null;
     
-/*
-### Advanced SQL Operations
 
-Task 13: Identify Members with Overdue Books
-Write a query to identify members who have overdue books (assume a 30-day return period). Display the member's name, book title, issue date, and days overdue.
+-- ### Advanced SQL Operations
+
+-- but first we add some recorde in table to perform other operations
+select * from issued_status;
+INSERT INTO issued_status(issued_id, issued_member_id, issued_book_name, issued_date, issued_book_isbn, issued_emp_id)
+VALUES
+('IS151', 'C118', 'The Catcher in the Rye', CURRENT_DATE - INTERVAL 24 DAY, '978-0-553-29698-2', 'E108'),
+('IS152', 'C119', 'The Catcher in the Rye', CURRENT_DATE - INTERVAL 13 DAY, '978-0-553-29698-2', 'E109'),
+('IS153', 'C106', 'Pride and Prejudice', CURRENT_DATE - INTERVAL 7 DAY, '978-0-14-143951-8', 'E107'),
+('IS154', 'C105', 'The Road', CURRENT_DATE - INTERVAL 32 DAY, '978-0-375-50167-0', 'E101');
+
+-- Adding new column in return_status
+select * from return_status;
+ALTER TABLE return_status
+ADD Column book_quality VARCHAR(15) DEFAULT('Good');
+
+-- Disable the safety lock
+SET SQL_SAFE_UPDATES = 0;
+
+UPDATE return_status
+SET book_quality = 'Damaged'
+WHERE issued_id IN ('IS112', 'IS117', 'IS118');
+
+-- Turn the safety lock back on
+SET SQL_SAFE_UPDATES = 1;
+
+SELECT * FROM return_status;
+
+-- Task 13: Identify Members with Overdue Books
+-- Write a query to identify members who have overdue books (assume a 30-day return period). Display the member's name, book title, issue date, and days overdue.
 
 
-Task 14: Update Book Status on Return
-Write a query to update the status of books in the books table to "available" when they are returned (based on entries in the return_status table).
+-- Task 14: Update Book Status on Return
+-- Write a query to update the status of books in the books table to "available" when they are returned (based on entries in the return_status table).
 
 
 
-Task 15: Branch Performance Report
-Create a query that generates a performance report for each branch, showing the number of books issued, the number of books returned, and the total revenue generated from book rentals.
+-- Task 15: Branch Performance Report
+-- Create a query that generates a performance report for each branch, showing the number of books issued, the number of books returned, and the total revenue generated from book rentals.
 
 
-Task 16: CTAS: Create a Table of Active Members
-Use the CREATE TABLE AS (CTAS) statement to create a new table active_members containing members who have issued at least one book in the last 6 months.
+-- Task 16: CTAS: Create a Table of Active Members
+-- Use the CREATE TABLE AS (CTAS) statement to create a new table active_members containing members who have issued at least one book in the last 6 months.
 
 
 
-Task 17: Find Employees with the Most Book Issues Processed
-Write a query to find the top 3 employees who have processed the most book issues. Display the employee name, number of books processed, and their branch.
+-- Task 17: Find Employees with the Most Book Issues Processed
+-- Write a query to find the top 3 employees who have processed the most book issues. Display the employee name, number of books processed, and their branch.
 
 
-Task 18: Identify Members Issuing High-Risk Books
-Write a query to identify members who have issued books more than twice with the status "damaged" in the books table. Display the member name, book title, and the number of times they've issued damaged books.    
+-- Task 18: Identify Members Issuing High-Risk Books
+-- Write a query to identify members who have issued books more than twice with the status "damaged" in the books table. Display the member name, book title, and the number of times they've issued damaged books.    
 
 
-Task 19: Stored Procedure
-Objective: Create a stored procedure to manage the status of books in a library system.
-    Description: Write a stored procedure that updates the status of a book based on its issuance or return. Specifically:
-    If a book is issued, the status should change to 'no'.
-    If a book is returned, the status should change to 'yes'.
+-- Task 19: Stored Procedure
+-- Objective: Create a stored procedure to manage the status of books in a library system.
+  --  Description: Write a stored procedure that updates the status of a book based on its issuance or return. Specifically:
+  -- If a book is issued, the status should change to 'no'.
+  -- a book is returned, the status should change to 'yes'.
 
-Task 20: Create Table As Select (CTAS)
-Objective: Create a CTAS (Create Table As Select) query to identify overdue books and calculate fines.
+-- Task 20: Create Table As Select (CTAS)
+-- Objective: Create a CTAS (Create Table As Select) query to identify overdue books and calculate fines.
 
-Description: Write a CTAS query to create a new table that lists each member and the books they have issued but not returned within 30 days. The table should include:
-    The number of overdue books.
-    The total fines, with each day's fine calculated at $0.50.
-    The number of books issued by each member.
-    The resulting table should show:
-    Member ID
-    Number of overdue books
-    Total fines
-*/
+-- Description: Write a CTAS query to create a new table that lists each member and the books they have issued but not returned within 30 days. The table should include:
+--    The number of overdue books.
+--    The total fines, with each day's fine calculated at $0.50.
+--    The number of books issued by each member.
+--    The resulting table should show:
+--    Member ID
+--    Number of overdue books
+--    Total fines
